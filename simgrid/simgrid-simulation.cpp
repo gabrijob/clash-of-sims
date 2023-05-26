326e1331f2fbacb945f8a1a7c0deabe043180bd7
gabrijob <gabrijob@gmail.com>
2023-05-26 15:42:56 +0200

    Makefile added

--- simgrid/simgrid-simulation.cpp
+++ simgrid/simgrid-simulation.cpp
@@ -1,15 +1,16 @@
 
-#include "simgrid/s4u.hpp"
+#include <simgrid/s4u.hpp>
 #include <vector>
+namespace sg4 = simgrid::s4u;
 
 XBT_LOG_NEW_DEFAULT_CATEGORY(s4u_test, "Messages specific for this s4u example");
 
-
 /* Static scheduler to distribute 100 equal tasks in a round-robin fashion. */
-static void scheduler_static(simgrid::s4u::Mailbox** mailbox_list, int num_workers) {
+static void scheduler_static(sg4::Mailbox** mailbox_list) {
   int num_tasks = 100;
   int wid = 0;
-  simgrid::s4u::Mailbox* worker_mailbox = nullptr;
+  sg4::Mailbox* worker_mailbox = nullptr;
+  int num_workers = sizeof(mailbox_list);
 
   for(int i=0; i < num_tasks; i++) {
     wid = i % num_workers;
@@ -36,12 +37,13 @@ static void scheduler_static(simgrid::s4u::Mailbox** mailbox_list, int num_worke
 }
 
 /* Basic worker that receives . */
-static void worker_basic(simgrid::s4u::Mailbox* mailbox) {
+static void worker_basic(sg4::Mailbox* mailbox) {
+  std::unique_ptr<double, std::default_delete<double> > sender_time; // Deve ter um jeito melhor de declarar essa linha
   do {
 
     XBT_INFO("Receiving from mailbox %s", mailbox->get_name().c_str());
     /* - Receive the task from scheduler ....*/
-    auto sender_time          = mailbox_in->get_unique<double>();
+    sender_time = mailbox->get_unique<double>();
 
     if (sender_time > 0) { 
       double communication_time = sg4::Engine::get_clock() - *sender_time;
@@ -59,26 +61,26 @@ static void worker_basic(simgrid::s4u::Mailbox* mailbox) {
 
 int main(int argc, char* argv[])
 {
-  simgrid::s4u::Engine e(&argc, argv);
+  sg4::Engine e(&argc, argv);
   e.load_platform(argv[1]);
 
   int num_workers = 4;
-  simgrid::s4u::Mailbox* mailboxes[num_workers];
+  sg4::Mailbox* mailboxes[num_workers];
 
   for (int i=1; i <= num_workers; i++) {
     // Create Mailbox for worker i
-    mailboxes[i] = e.mailbox_by_name_or_create("MAILBOX_W%d", i);
+    mailboxes[i] = sg4::Mailbox::by_name(std::string("MAILBOX_W%d", i));
 
     // Create Actor for worker i
-    simgrid::s4u::Actor::create("worker", e.host_by_name("HOST_%d", i), worker_basic, mailboxes[i]);
+    sg4::Actor::create("worker", e.host_by_name(std::string("HOST_%d", i)), worker_basic, mailboxes[i]);
   }
 
   // Create Actor for scheduler
-  simgrid::s4u::Actor::create("scheduler", e.host_by_name("HOST_%d", num_workers+1), scheduler_static, mailboxes, num_workers);
+  sg4::Actor::create("scheduler", e.host_by_name(std::string("HOST_%d", num_workers+1)), scheduler_static, mailboxes);
 
   e.run();
 
-  XBT_INFO("Simulation time %g", simgrid::s4u::Engine::get_clock());
+  XBT_INFO("Simulation time %g", sg4::Engine::get_clock());
 
   return 0;
 }
